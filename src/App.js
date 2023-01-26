cc36c8517b8cbf9b3d62ffff010b7dbef6782e6f
GianniMS <1008999@hr.nl>
2023-01-26 15:01:35 +0100

    Create, read, delete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import {Note} from "./Note";
+import {NewNote} from "./NewNote";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URI_COLLECTION = "https://docent.cmi.hro.nl/bootb/demo/notes"
+
+export function App() {
+    const [notes, setNotes] = useState([]);
+
+    const loadJson = () => {
+        fetch(URI_COLLECTION, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+            .then((response) => response. json())
+            .then((result) => setNotes(result.items))
+    }
+
+    const showNotes = notes.map((value, key) =>
+        <Note key={value.id} note={value} notesRefreshHandler={loadJson} />)
+
+    useEffect(loadJson, [])
+
+    return <div>
+        <h1>Hello Notes!</h1>
+      {showNotes}
+      <NewNote notesRefreshHandler={loadJson}/>
+    </div>;
 }
-
-export default App;
